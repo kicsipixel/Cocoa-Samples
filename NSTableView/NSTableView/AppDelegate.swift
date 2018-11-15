80710331d74b9fa48d952cb7f4277999f51008d9
Szabolcs Toth <tsz@purzelbaum.hu>
2018-11-15 16:03:43 +0400

    Finished TableView.

---
+++ NSTableView/NSTableView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTableView
+//
+//  Created by Szabolcs Toth on 11/15/18.
+//  Copyright © 2018 purzelbaum.hu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTableView/NSTableView/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  NSTableView
+//
+//  Created by Szabolcs Toth on 11/15/18.
+//  Copyright © 2018 purzelbaum.hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet var tableView: NSTableView!
+    
+    // our variable
+    var data: [[String: String]] = [[:]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // adding people
+        data = [
+            [
+                "firstName" : "Ragnar",
+                "lastName" : "Lothbrok",
+                "mobileNumber" : "555-1234"
+            ],
+            [
+                "firstName" : "Bjorn",
+                "lastName" : "Lothbrok",
+                "mobileNumber" : "555-3412"
+            ],
+            [
+                "firstName" : "Harald",
+                "lastName" : "Finehair",
+                "mobileNumber" : "555-4512"
+            ]
+        ]
+        
+        // reload tableview
+        tableView.reloadData()
+    }
+
+}
+
+extension ViewController: NSTableViewDataSource, NSTableViewDelegate {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return (data.count)
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let person = data[row]
+        
+        let cell = tableView.makeView(withIdentifier: (tableColumn!.identifier), owner: self) as? NSTableCellView
+        cell?.textField?.stringValue = person[tableColumn!.identifier.rawValue]!
+        
+        return cell
+    }
+}
+
+
